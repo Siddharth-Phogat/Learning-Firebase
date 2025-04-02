2e3447a20d40c2be19fba537175615aa04fa40ac
Siddharth_Phogat <siddharthphogat22@gmail.com>
2025-04-02 17:11:07 +0530

    add auth demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import './App.css'
-import { getDatabase, ref, set } from 'firebase/database'
-import { app } from './firebase'
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { app } from "./firebase";
+import "./App.css";
+
+const auth = getAuth(app);
 
 function App() {
+  const signUpUser = () => {
+    createUserWithEmailAndPassword(
+      auth,
+      "siddharthphogat@gmail.com",
+      "siddharth@123"
+    )
+    .then((value) => console.log(value));
+  };
 
   return (
     <>
-      <div className='text-4xl flex justify-center'>
+      <div className="text-4xl flex justify-center">
         <h1>Firebase React App</h1>
         <div>
-          <button className='bg-blue-500 p-3 rounded-2xl text-white' onClick={putData}>Put data</button>
+          <button
+            className="bg-blue-500 p-3 rounded-2xl text-white"
+            onClick={signUpUser}
+          >
+            Create User
+          </button>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
